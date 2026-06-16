a04a028d1e822290fb2e0b6aeb324a9d776338cd
ARoskelley <aroskelley2112@gmail.com>
2026-06-15 20:30:21 -0600

    [DARCI checkpoint] task=560b8c2464ba41e7aa4557b2c64f84f6: Pre-run checkpoint for task: Implement the Damm check-digit …

---
+++ tmp/dammproject/Tests/BehaviorCheck.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace DammProject.Tests;
+
+public class BehaviorCheck
+{
+    [Fact]
+    public void ComputeCheckDigit_ThrowsArgumentException_ForNonDigitCharacters()
+    {
+        Assert.Throws<ArgumentException>(() => DammValidator.ComputeCheckDigit("572a"));
+    }
+
+    [Fact]
+    public void IsValid_ReturnsFalse_ForEmptyString()
+    {
+        Assert.False(DammValidator.IsValid(""));
+    }
+}
